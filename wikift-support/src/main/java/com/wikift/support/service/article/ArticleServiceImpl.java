cac3f7c84468981dd2ad36a1f6f61d6bf8532901
shicheng <shicheng@ttxit.com>
2018-05-02 13:53:10 +0800

    add the default browse of the article system

--- wikift-support/src/main/java/com/wikift/support/service/article/ArticleServiceImpl.java
+++ wikift-support/src/main/java/com/wikift/support/service/article/ArticleServiceImpl.java
@@ -28,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
@@ -91,7 +92,10 @@ public class ArticleServiceImpl implements ArticleService {
     }
 
     @Override
+    @Transactional
     public ArticleEntity getArticle(Long id) {
+        // 设置文章浏览量
+        articleRepository.viewArticle(id, 1);
         return articleRepository.findById(id);
     }
 
